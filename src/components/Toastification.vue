41177918d757441ebcc33eebe156649fff5943ca
Issei0813star <issei0813star.dev@gmail.com>
2024-09-30 23:34:46 +0900

    toastificationラッパーコンポーネント(途中)

---
+++ src/components/Toastification.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { useToast } from 'vue-toastification'
+
+export default defineComponent ({
+  setup() {
+    const toast = useToast();
+
+    const showError = (message:string) => {
+      toast.error(message);
+    }
+
+    const showSuccess = (message:string) => {
+      toast.success(message);
+    }
+  }
+})
+</script>
